61618c89440b41d6d0dac4355c2a9bd4a26393e1
Vinicius <vinicius_dogma@hotmail.com>
2020-06-30 11:05:49 -0300

    node challenge complete

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +9,48 @@ app.use(cors());
 
 const repositories = [];
 
+function validatedId(request, response, next) {
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex((repo) => repo.id === id);
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: "Repository not found." });
+  }
+  request.index = repositoryIndex;
+  next();
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+  repositories.push(repository);
+  response.json(repository);
 });
 
+app.use("/repositories/:id", validatedId);
+
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { index } = request;
+  const { title, url, techs } = request.body;
+  repositories[index].title = title ? title : repositories[index].title;
+  repositories[index].url = url ? url : repositories[index].url;
+  repositories[index].techs = techs ? techs : repositories[index].techs;
+  response.json(repositories[index]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { index } = request;
+  repositories.splice(index, 1);
+  response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { index } = request;
+  repositories[index].likes = repositories[index].likes + 1;
+  response.json(repositories[index]);
 });
 
 module.exports = app;
